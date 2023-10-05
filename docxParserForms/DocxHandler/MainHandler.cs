2c72c49f745c22a0760630b8a4fd8f01488fa673
bondarevalexei <gtrompy@gmail.com>
2023-10-05 13:12:57 +0300

    add reading method

---
+++ docxParserForms/DocxHandler/MainHandler.cs
@@ -0,0 +1,28 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace docxParserForms.DocxHandler
+{
+    public class MainHandler
+    {
+        public string ReadText(string filepath)
+        {
+            string text = String.Empty;
+
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(filepath, false))
+            {
+                Body body = wordDocument.MainDocumentPart.Document.Body;
+                foreach(var paragraph in body.Elements<Paragraph>())
+                {
+                    foreach (var run in paragraph.Elements<Run>())
+                    {
+                        text += run.InnerText;
+                    }
+                    text += Environment.NewLine;
+                }
+            }
+
+            return text;
+        }
+    }
+}
